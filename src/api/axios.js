75826c8df271b755ccf7e81361de70980f518dbd
bharti-kanwar97 <kanwarbharti96@gmail.com>
2026-05-27 12:21:52 +0530

    add url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/v1'
+    // baseURL: 'http://localhost:3000/api/v1'
+    baseURL: 'https://backend-academy.vercel.app'
 })
 export default api;
