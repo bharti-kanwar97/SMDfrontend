d99a057758360a036b5eb4b31a2978bd313d5b8d
bharti-kanwar97 <kanwarbharti96@gmail.com>
2026-05-28 22:17:34 +0530

    courseDetail

--- src/components/DropdownMenu2.jsx
+++ src/components/DropdownMenu2.jsx
@@ -8,13 +8,13 @@ const [subMenu, setSubMenu] = useState(false)
   return (
 
     <>
-  { open && <div className='sideMenu flex flex-col fixed w-full lg:top-[40px] sm:top-[95px] top-[58px] shadow-xl/45 left-0 bg-white px-12 py-4 sm:py-5 lg:py-8 z-2 block lg:hidden max-h-fit overflow-y-auto'>
+  { open && <div className='sideMenu flex flex-col fixed w-full lg:top-[40px] sm:top-[95px] top-[58px] shadow-xl/45 left-0 bg-white px-12 py-4 sm:py-5 lg:py-8 z-2 block lg:hidden h-[calc(100vh-58px)] overflow-y-auto '>
       <ul className='flex flex-col gap-4'> 
         <li className='font-sans font-normal text-[#888] text-[16px] leading-[18px] text-left py-[16px] border-b-2 border-dotted'  onClick={() => {setOpen(false); setSideMenu(false)} }><Link to='/'> Home</Link></li>
         <li className='font-sans font-normal text-[#888] text-[16px] leading-[18px] text-left py-[16px] border-b-2 border-dotted' onClick={() => {setOpen(false); setSideMenu(false)} }><Link to='/about'> About</Link></li>
         <li className={`font-sans font-normal text-[#888] text-[16px] text-left leading-[18px] pt-[16px]
       ${subMenu
-    ? "pb-none"
+    ? "pb-0"
     : "pb-[16px] border-b-2 border-dotted"
   }`} onClick={() => {setOpen(true); setSubMenu(prev => !prev) }}>Course <IoMdArrowDropdown className="inline-block" />
         
