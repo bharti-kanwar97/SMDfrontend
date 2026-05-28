316990219204ccfe60860b7e446e375354d9177e
bharti-kanwar97 <kanwarbharti96@gmail.com>
2026-05-28 21:59:30 +0530

    courseDetail

--- src/components/DropdownMenu2.jsx
+++ src/components/DropdownMenu2.jsx
@@ -8,7 +8,7 @@ const [subMenu, setSubMenu] = useState(false)
   return (
 
     <>
-  { open && <div className='sideMenu flex flex-col fixed w-full lg:top-[40px] sm:top-[95px] top-[58px] shadow-xl/45 left-0 bg-white px-12 py-4 sm:py-5 lg:py-8 z-2 block lg:hidden'>
+  { open && <div className='sideMenu flex flex-col fixed w-full lg:top-[40px] sm:top-[95px] top-[58px] shadow-xl/45 left-0 bg-white px-12 py-4 sm:py-5 lg:py-8 z-2 block lg:hidden max-h-screen overflow-y-auto'>
       <ul className='flex flex-col gap-4'> 
         <li className='font-sans font-normal text-[#888] text-[16px] leading-[18px] text-left py-[16px] border-b-2 border-dotted'  onClick={() => {setOpen(false); setSideMenu(false)} }><Link to='/'> Home</Link></li>
         <li className='font-sans font-normal text-[#888] text-[16px] leading-[18px] text-left py-[16px] border-b-2 border-dotted' onClick={() => {setOpen(false); setSideMenu(false)} }><Link to='/about'> About</Link></li>
