815ebb2fdafea6c289fd4f75471fe9daef4c2fd6
bharti-kanwar97 <kanwarbharti96@gmail.com>
2026-05-29 23:20:55 +0530

    perform real time validation

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -41,6 +41,7 @@ export default function ContactForm() {
       }
       break;
 
+
     case "msg":
       if (!value.trim()) {
         error = "Message is required";
@@ -117,7 +118,7 @@ export default function ContactForm() {
           value={formData.name}
           onChange={handleChange}
         />
-        {error.name && <p className="text-red-500 text-xs">{error.name}</p>}
+        {error.name && <p className="text-red-500">{error.name}</p>}
         <br />
         <input
           type="email"
