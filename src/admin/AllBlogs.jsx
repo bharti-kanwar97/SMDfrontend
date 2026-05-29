a74f032a3143fa70ceb07c13bd7d4a3181552ac7
bharti-kanwar97 <kanwarbharti96@gmail.com>
2026-05-30 00:14:53 +0530

    perform real time validation

--- src/admin/AllBlogs.jsx
+++ src/admin/AllBlogs.jsx
@@ -36,7 +36,7 @@ export default function AllBlogs() {
   }, []);
   return (
     <>
-      <div className="bg-[#C9C5B1]">
+      <div className="bg-[#C9C5B1] h-screen flex flex-col">
         <div className="relative flex justify-around items-center">
           <div className="md:invisible bg-red text-[25px] px-2.5 text-[#3b0440] py-2 hover:cursor-pointer">
             <GiHamburgerMenu />
@@ -46,11 +46,11 @@ export default function AllBlogs() {
             <Create />
           </Link>
         </div>
-         <div className="flex gap-4">
-           <div className="w-[30%]">
+         <div className="flex flex-1 overflow-hidden gap-4 ">
+           <div className="w-[30%] h-full overflow-y-auto">
             <SideNav />
           </div>
-        <div className="bg-[#C9C5B1] w-full h-screen">
+        <div className="bg-[#C9C5B1] w-full  h-full overflow-y-auto">
           <table className="table-auto text-center max-w-[1200px] min-w-[600px] w-[100%] mx-auto bg-white rounded-xl shadow-xl/80">
             <thead>
               <tr className="text-2xl bg-[#3b0440] border-b-2 border-zinc-800  ">
