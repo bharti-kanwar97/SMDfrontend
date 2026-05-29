22a92daa0a60fe3a158149faec7295a19839ac80
bharti-kanwar97 <kanwarbharti96@gmail.com>
2026-05-30 00:05:28 +0530

    perform real time validation

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -8,11 +8,11 @@ const Dashboard = () => {
       <div className="h-screen flex flex-col ">
         
         <Header />
-        <div className="flex gap-8 bg-[#EDE8D0] ">
-          <div className="w-[30%]">
+        <div className="flex flex-1 overflow-hidden gap-8 bg-[#EDE8D0] ">
+          <div className="w-[30%] h-full overflow-y-auto">
             <SideNav />
           </div>
-          <div className="w-full bg-[#EDE8D0] px-10 py-10">
+          <div className="w-full bg-[#EDE8D0] px-10 py-10flex-1 h-full overflow-y-auto">
             <div className="flex gap-5 justify-left ">
               <Link to="/admin/allUsers">
                 <div className="w-[250px]  h-[250px] text-center py-[75px] bg-white rounded-md text-[24px] font-semibold hover:cursor-pointer hover:transition-all hover:translate-y-3 hover:shadow-xl/30 hover:duration-500 hover:delay-200 hover:ease-in-out ease-in-out">
